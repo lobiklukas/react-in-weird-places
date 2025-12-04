d1fa696507525c075a6f1085dc4b2f0df058a655
Lukáš Lobík <lobiklukas@gmail.com>
2025-12-04 18:56:54 +0100

    Update ports

--- packages/presentation/vite.config.ts
+++ packages/presentation/vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [svelte()],
+	server: {
+		port: 6000,
+	},
+	plugins: [svelte(), tailwindcss()],
 	resolve: {
 		alias: {
 			'@config': path.resolve(__dirname, './src/config.ts'),
